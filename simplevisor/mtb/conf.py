af8d5e5b325d844aa4fa4b1f3aa901aff5bed424
Massimo <massimo.paladin@gmail.com>
2013-02-04 17:29:32 +0100

    updating share code

--- simplevisor/mtb/conf.py
+++ simplevisor/mtb/conf.py
@@ -4,9 +4,11 @@ Configuration utilities for :py:mod:`mtb` module.
 
 Copyright (C) 2013 CERN
 """
+import re
+from subprocess import Popen, PIPE
+
 from mtb.modules import json
 from mtb import PY2, PY3
-from subprocess import Popen, PIPE
 
 
 def _normalize_bool(tree):
@@ -57,3 +59,141 @@ def unify_keys(dictionary):
         if type(tmp) is dict:
             unify_keys(tmp)
     return dictionary
+
+
+_EXPLOSION_RE = re.compile("^(\w+)-(.+)$")
+
+
+def _explode_dict(given):
+    """ Explode all the keys having a "-". """
+    for key in given.keys():
+        match = _EXPLOSION_RE.match(key)
+        if match:
+            if match.group(1) in given:
+                given[match.group(1)].update(
+                    {match.group(2): given.pop(key)})
+            else:
+                given[match.group(1)] = {
+                    match.group(2): given.pop(key)}
+    for item in given.values():
+        if type(item) == dict:
+            _explode_dict(item)
+
+
+def _tree_dictify(given):
+    """ TreeDict-ify it. """
+    for key, item in given.items():
+        if type(item) == dict:
+            _tree_dictify(item)
+            given[key] = TreeDict(item)
+
+
+class TreeDict(object):
+    """ Exploded dict. """
+
+    def __init__(self, _dict=None):
+        if _dict is None:
+            self._dict = dict()
+        else:
+            self._dict = _dict
+            _explode_dict(self._dict)
+            _tree_dictify(self._dict)
+
+    def __contains__(self, key):
+        return self._dict.__contains__(key)
+
+    def __getitem__(self, key):
+        match = _EXPLOSION_RE.match(key)
+        if match is None or match.group(1) not in self._dict:
+            return self._dict[key]
+        value = self._dict[match.group(1)]
+        if type(value) is dict:
+            return TreeDict(value)[match.group(2)]
+        elif type(value) is TreeDict:
+            return value[match.group(2)]
+        raise KeyError("key not present: %s" % (key, ))
+
+    def get(self, key, default=None):
+        """
+        Return the value pointed by the given key,
+        it the item is not found it returns the default value.
+        If default is not provided it will return None.
+        """
+        match = _EXPLOSION_RE.match(key)
+        if match is None:
+            return self._dict.get(key, default)
+        elif match.group(1) not in self._dict:
+            return default
+        # match.group(1) in self._dict
+        value = self._dict[match.group(1)]
+        if type(value) is dict:
+            return TreeDict(value).get(match.group(2), default)
+        elif type(value) is TreeDict:
+            return value.get(match.group(2), default)
+        return default
+
+    def pop(self, key, default=None):
+        """
+        Remove and return the value pointed by the given key,
+        it the item is not found it returns the default value.
+        If default is not provided it will return None.
+        """
+        match = _EXPLOSION_RE.match(key)
+        if match is None:
+            return self._dict.pop(key, default)
+        elif match.group(1) not in self._dict:
+            return default
+        # match.group(1) in self._dict
+        value = self._dict.pop(match.group(1), default)
+        if type(value) is dict:
+            return TreeDict(value).pop(match.group(2), default)
+        elif type(value) is TreeDict:
+            return value.pop(match.group(2), default)
+        return default
+
+    def __setitem__(self, key, val):
+        match = _EXPLOSION_RE.match(key)
+        if match is None:
+            self._dict[key] = val
+        else:
+            if match.group(1) not in self._dict:
+                self._dict[match.group(1)] = TreeDict()
+            sub_value = self._dict[match.group(1)]
+            sub_value[match.group(2)] = val
+
+    def setdefault(self, key, default=None):
+        """
+        Set the item identified by key to default value,
+        if default is not provided the item will be set to None.
+        """
+        match = _EXPLOSION_RE.match(key)
+        if match is None:
+            return self._dict.setdefault(key, default)
+        else:
+            val = self._dict.setdefault(match.group(1), TreeDict())
+            val.setdefault(match.group(2), default)
+
+    def keys(self):
+        """
+        Return the set of keys.
+        """
+        return self._dict.keys()
+
+    def items(self):
+        """
+        Return the set of items.
+        """
+        return self._dict.items()
+
+    def update(self, *args, **kwargs):
+        """
+        Update TreeDict with given values.
+        """
+        return self._dict.update(*args, **kwargs)
+
+    def copy(self):
+        """ Return a copy of it. """
+        return TreeDict(self._dict)
+
+    def __repr__(self):
+        return "%s" % (self._dict, )
